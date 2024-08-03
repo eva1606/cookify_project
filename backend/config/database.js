6763943d4021f1c4ecffc62c3a93c71fe3540633
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-08-03 04:48:17 +0300

    update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,21 +1,11 @@
-const mysql = require('mysql2');
-require('dotenv').config({ path: __dirname + '/.env'});
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const connection = mysql.createConnection
-({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
- });
+});
 
- connection.connect(err => {
-    if(eer)
-    {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database');
- });
-
- module.exports = connection;
+module.exports = pool;
