ee015a58a4c6a7455b1867fe41d3f4171f795aeb
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-08-03 04:59:51 +0300

    update server.js and add api login

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+
+const loginRoutes = require('./routes/loginRoutes');
+app.use('/api/auth', loginRoutes);  
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/login.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
