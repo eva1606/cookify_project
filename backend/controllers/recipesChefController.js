64bd5d53d65f46966d4a95660cf9cadecbf83c5c
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-08-03 16:46:55 +0300

    Initial commit with basic CRUD functions setup

---
+++ backend/controllers/recipesChefController.js
@@ -0,0 +1,28 @@
+const pool = require('..config/database.js');
+
+const addRecipe = async (req, res) => 
+{
+    
+};
+
+const getRecipesByChef = async (req, res) => 
+{
+    
+};
+
+const deleteRecipe = async (req, res) => 
+{
+   
+};
+
+const updateRecipe = async (req, res) => 
+{
+    
+};
+
+module.exports = {
+    addRecipe,
+    getRecipesByChef,
+    deleteRecipe,
+    updateRecipe
+};
