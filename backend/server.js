af565e5babc9ff7d424603b4283c512ca4ed2e7e
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-08-03 19:26:46 +0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
 const path = require('path'); 
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
+const fs = require('fs');
 const app = express();
-
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir);
+}
 
 const loginRoutes = require('./routes/loginRoutes');
 const homeUserRoutes = require('./routes/homeUserRoutes');
-
+const chefRoutes = require('./routes/RecipesChefRoutes'); // Chemin corrigé
 
 app.use('/api/auth', loginRoutes);  
 app.use('/api', homeUserRoutes);
+app.use('/api/chef', chefRoutes);
 
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/login.html'));
 });
