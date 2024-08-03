40fdd40b6c77f672f35a4b219ccaae1f8c1c6fe3
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-08-03 16:15:35 +0300

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
+const path = require('path'); 
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
-const path = require('path');
 const app = express();
-require('dotenv').config();
+
 
 app.use(express.json());
-
-
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 
